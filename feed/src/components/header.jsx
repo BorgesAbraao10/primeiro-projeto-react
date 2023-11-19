3478c03f6fe0e56b54b5fb82e58b54ae334a7a8d
Abraão Borges <abraao10clear@gmail.com>
2023-11-19 14:59:32 -0300

    last update

--- feed/src/components/header.jsx
+++ feed/src/components/header.jsx
@@ -1,8 +1,11 @@
 import style from "./Header.module.css";
 
+import igniteLogo from "../assets/ignite-logo.svg";
+
 export function Header() {
   return (
     <header className={style.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
       <strong>Ignite Feed</strong>
     </header>
   );

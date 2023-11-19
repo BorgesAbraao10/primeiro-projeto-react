df8c65533b1ef1284de49ac94d3438d314f8c84a
Abraão Borges <abraao10clear@gmail.com>
2023-11-19 14:14:36 -0300

    last update

--- feed/src/components/header.jsx
+++ feed/src/components/header.jsx
@@ -2,8 +2,8 @@ import style from "./Header.module.css";
 
 export function Header() {
   return (
-  <header className={style.header}>
-    <strong>Ignite Feed</strong>;
-  </header>
+    <header className={style.header}>
+      <strong>Ignite Feed</strong>
+    </header>
   );
 }

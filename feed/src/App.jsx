9254b6bf433fd24fe054a5e684514ac672d02869
Abraão Borges <abraao10clear@gmail.com>
2023-11-19 20:21:46 -0300

    last update

--- feed/src/App.jsx
+++ feed/src/App.jsx
@@ -1,12 +1,19 @@
 import { Post } from "./Post";
 import { Header } from "./components/header";
 
+import style from "./App.module.css";
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={style.wrapper}></div>
+      <aside>
+        sidebar
+      </aside>
+      <main>
       <Post
         author="Abraão Borges"
         content="Sou um estudante de programação apaixonado por criar soluções para problemas complexos usando tecnologia."
@@ -15,6 +22,7 @@ export function App() {
         author="Albert Einsten"
         content="Diante de Deus todos somos igualmente sábios e igualmente tolos."
       />
+      </main>
     </div>
   );
 }
